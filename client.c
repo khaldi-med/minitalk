4d18802523e33f76d134f293cd1fb8f1b5798acd
mohkhald <mohkhald@student.1337.ma>
2025-02-26 19:09:01 +0000

    mohkhald

--- client.c
+++ client.c
@@ -14,7 +14,7 @@ void	to_bit(int pid, char c)
 	while (i < 8)
 	{
 		res = (c >> i) & 1;
-		if (res == 0)
+		if (res)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
@@ -24,27 +24,29 @@ void	to_bit(int pid, char c)
 
 int	main(int ac, char **av)
 {
-	int	i;
-	int	pid;
+	int		i;
+	int		pid;
+	char	*str;
 
 	i = 0;
 	if (ac == 3)
 	{
+		str = av[2];
 		pid = atoi(av[1]);
-		if (!(pid <= 9 && pid >= 0))
+		if (pid < 0)
 		{
-			write(1, "The argements not valide!", 24);
-			exit(0);
+			write(1, "The PID not valide!", 24);
+			return (1);
 		}
-		while (av[2][i])
+		while (*str)
 		{
-			to_bit(pid, av[2][i]);
-			i++;
+			to_bit(pid, *str);
+			str++;
 		}
 	}
 	else
 	{
 		write(1, "The argements not valide!", 24);
-		exit(0);
+		return (1);
 	}
 }
